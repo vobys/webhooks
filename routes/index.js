a8b6a4408ad6cd03bf9cd546af0ad292b5b535b6
Alfredo Fialho <alfoltran@gmail.com>
2020-03-16 15:49:29 -0300

    :bookmark: Atualizando versão.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  res.render("index", { title: "WebHooks Server", version: "0.1.0" });
+  res.render("index", { title: "WebHooks Server", version: "0.2.0" });
 });
 
 module.exports = router;
