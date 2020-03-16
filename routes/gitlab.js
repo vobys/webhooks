35595a54eb63798e02b08c1b538aeea233f82628
Alfredo Fialho <alfoltran@gmail.com>
2020-03-16 14:56:02 -0300

    :sparkles: Adicionando notas com resumo do SonarQube. Notas são adicionandas ao "merge request" caso exista.

--- routes/gitlab.js
+++ routes/gitlab.js
@@ -5,6 +5,46 @@ const router = express.Router();
 
 const config = require("../config.json");
 
+const rateMeasure = ["-", "A", "B", "C", "D", "E"];
+
+const measures = {
+  // eslint-disable-next-line camelcase
+  reliability_rating: {
+    label: "Reliability Rating",
+    icon: "spider",
+    value: num => rateMeasure[num]
+  },
+  // eslint-disable-next-line camelcase
+  security_rating: {
+    label: "Security Rating",
+    icon: "unlock",
+    value: num => rateMeasure[num]
+  },
+  // eslint-disable-next-line camelcase
+  sqale_rating: {
+    label: "Maintainability Rating",
+    icon: "radioactive",
+    value: num => rateMeasure[num]
+  },
+  coverage: {
+    label: "Coverage",
+    icon: "thermometer",
+    value: num => num + "%"
+  },
+  // eslint-disable-next-line camelcase
+  duplicated_lines_density: {
+    label: "Duplicated lines",
+    icon: "page_facing_up",
+    value: num => num + "%"
+  },
+  // eslint-disable-next-line camelcase
+  test_success_density: {
+    label: "Unit test success density",
+    icon: "eye",
+    value: num => num + "%"
+  }
+};
+
 // eslint-disable-next-line max-params
 function createStatus(status, project, url, sha, ref, coverage) {
   const options = {
@@ -30,6 +70,61 @@ function createStatus(status, project, url, sha, ref, coverage) {
       "PRIVATE-TOKEN": config.gitlab.token,
       "Content-Type": "application/json"
     },
+    json: true
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+}
+
+// eslint-disable-next-line max-params
+function createMergeNote(project, ref, qualityGate, projectKey, serverUrl) {
+  const options = {
+    method: "GET",
+    url:
+      "https://gitlab.com/api/v4/projects/" +
+      project +
+      "/merge_requests?target_branch=master&source_branch=" +
+      ref,
+    headers: {
+      "User-Agent": "WebHooks-App",
+      "cache-control": "no-cache",
+      "PRIVATE-TOKEN": config.gitlab.token,
+      "Content-Type": "application/json"
+    },
+    json: true
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    if (body.length > 0 && body[0].iid) {
+      createNote(
+        extractNote(qualityGate, projectKey, serverUrl),
+        project,
+        body[0].iid
+      );
+    }
+  });
+}
+
+function createNote(note, project, merge) {
+  const options = {
+    method: "POST",
+    url:
+      "https://gitlab.com/api/v4/projects/" +
+      project +
+      "/merge_requests/" +
+      merge +
+      "/notes",
+    headers: {
+      "User-Agent": "WebHooks-App",
+      "cache-control": "no-cache",
+      "PRIVATE-TOKEN": config.gitlab.token,
+      "Content-Type": "application/json"
+    },
+    body: { body: note },
     json: true
   };
 
@@ -68,6 +163,25 @@ function extractBranch(sonar) {
   return sonar.branch.name;
 }
 
+function extractNote(qualityGate, project, serverUrl) {
+  let note =
+    "### SonarQube\n" +
+    ":construction_worker: Continuous Code Quality (" +
+    qualityGate.name +
+    ")\n";
+  qualityGate.conditions
+    .filter(condition => measures[condition.metric])
+    .forEach(condition => {
+      note += `>\n> ${
+        condition.status === "OK" ? ":green_heart:" : ":heart:"
+      } ${measures[condition.metric].label} :${
+        measures[condition.metric].icon
+      }: \`${measures[condition.metric].value(condition.value)}\`\n`;
+    });
+  note += `\n[![Quality Gate Status](${serverUrl}/api/project_badges/measure?project=${project.key}&metric=alert_status)](${project.url})`;
+  return note;
+}
+
 router.post("/sonar/status", function(req, res) {
   const sonar = req.body;
   let status;
@@ -84,6 +198,13 @@ router.post("/sonar/status", function(req, res) {
     sonar.revision,
     extractBranch(sonar),
     extractCoverage(sonar.qualityGate)
+  );
+  createMergeNote(
+    config.gitlab.namespace + "%2F" + sonar.project.key,
+    extractBranch(sonar),
+    sonar.qualityGate,
+    sonar.project,
+    sonar.serverUrl
   );
   res.send({ status: status });
 });
