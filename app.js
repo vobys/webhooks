323abf8b2226350d6dbc33bc219ae5f158a95c9c
Alfredo Fialho <alfoltran@gmail.com>
2021-01-15 16:29:03 -0300

    :sparkles: :construction: Adicionando "endpoint" para o deploy.

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const githubRouter = require("./routes/github");
 const gitlabRouter = require("./routes/gitlab");
 const teamsRouter = require("./routes/teams");
+const deployRouter = require("./routes/deploy");
 
 const app = express();
 
@@ -23,10 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/github", githubRouter);
 app.use("/gitlab", gitlabRouter);
 app.use("/teams", teamsRouter);
+app.use("/deploy", deployRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/deploy.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const request = require("request");
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+const config = require("../config.json");
+
+function checkStatus(repo, callback) {
+  const options = {
+    method: "GET",
+    url: `https://api.github.com/repos/${config.github.owner}/${repo}/commits/master/check-runs`,
+    headers: {
+      "User-Agent": "Vobys WebHook Server",
+      "Content-Type": "application/json",
+      Authorization: `token ${config.github.token}`,
+      Accept: "application/vnd.github.antiope-preview+json"
+    }
+  };
+
+  return request(options, callback);
+}
+
+router.get("/github/:repo/:environment", function(req, res) {
+  let error = "";
+  let server = "";
+  let url = "";
+
+  checkStatus(req.params.repo, function(err, response) {
+    if (err) error = err;
+    // eslint-disable-next-line no-negated-condition
+    else if (response.statusCode !== 200) error = response.statusMessage;
+    else {
+      const check = JSON.parse(response.body).check_runs;
+      if (
+        check.length > 0 &&
+        check[0].status === "completed" &&
+        check[0].conclusion === "success"
+      ) {
+        server = `Start deploy to ${req.params.environment} now!`;
+        url = `/deploy/github/${req.params.repo}/${req.params.environment}/${check[0].head_sha}`;
+      } else {
+        error = "master is not ready";
+      }
+    }
+
+    res.render("deploy", {
+      title: "Deploy to Server",
+      error: error,
+      server: server,
+      url: url
+    });
+  });
+});
+
+module.exports = router;
